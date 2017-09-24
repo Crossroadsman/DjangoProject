6c8d64f3888eebdea7bb00aa93e6a9d6c6ff31b1
Crossroadsman <crossroadsman@gmail.com>
2017-09-23 22:30:17 -0600

    pip install django

---
+++ djangoproject_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alex/python/django/djangoproject_polls/djangoproject_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
